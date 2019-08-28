885c032bf58e0014cf7e1d10cc2048b409eca12b
vadik <vadik.org@gmail.com>
2019-08-28 10:12:30 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   fish_variables
    	new file:   functions/fish_prompt.fish
    	new file:   functions/less.fish
    	new file:   functions/man.fish

---
+++ functions/man.fish
@@ -0,0 +1,7 @@
+set -x LESS_TERMCAP_mb (set_color red)
+set -x LESS_TERMCAP_md (set_color green)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_se (set_color normal)
+set -x LESS_TERMCAP_so (set_color -b green bryellow)
+set -x LESS_TERMCAP_ue (set_color normal)
+set -x LESS_TERMCAP_us (set_color blue)
